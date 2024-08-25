6b52097aed8a29d91caab41a1a9b4c32eeb41296
Madiha Fatima <fmadiha252@gmail.com>
2024-08-25 07:31:41 -0700

    port#5000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const connectToMongo = require('./db'); // Adjust if necessary
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 async function startServer() {
     try {
